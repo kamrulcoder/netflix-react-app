1ad0e3ac5aa5ec0d129cae8776c85935a24c300c
kamrulcoder <kamrulcoder@gmail.com>
2021-11-04 23:50:42 -0700

    all file chages

--- api/index.js
+++ api/index.js
@@ -1,86 +1,32 @@
-// express import 
 const express = require("express");
-
-// app create 
 const app = express();
-
-//  all dependency import 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const morgan = require("morgan")
-const cors = require('cors');
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
-// dependency import 
-const middleware = [
-    morgan('dev'),
-    express.static('public'),
-    express.urlencoded({ extended: true }),
-    express.json(),
-    cors()
-]
-/* ==========================
-    all route import   start 
-*============================ */
-
-
-
-/* ==========================
-    all route import   start 
-*============================ */
-
-
-// env dependency  function call 
 dotenv.config();
-app.use(middleware)
 
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
 
-/* ==========================
-    all route  use start  
-*============================ */
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("<h2>you are success and all  work api project </h2>")
-})
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
-/* ==========================
-    all route  use end   
-*============================ */
-
-
-
-/* ==========================
- mongdb connect code  start 
-*============================ */
-
-const PORT = process.env.PORT || 5000;
-
-const uri = process.env.MONGODB_URL;
-mongoose.connect(uri,
-    { useNewUrlParser: true })
-    .then(() => {
-        console.log('Database Connected')
-        app.listen(PORT, () => {
-            console.log(`Server is running on PORT ${PORT}`)
-        })
-    })
-    .catch(e => {
-        return console.log(e)
-    })
-
-
-/* ==========================
- mongdb connect code  end
-*============================ */
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
--- api/models/User.js
+++ api/models/User.js
@@ -1,33 +1,14 @@
-const { Schema, model } = require('mongoose')
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePic: { type: String, defaut: "" },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
-const userScheema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    profilePic: {
-        type: String, default: ""
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    }
-},
-
-    {
-        timestamps: true
-    }
-)
-
-module.exports = model("User", userScheema)
+module.exports = mongoose.model("User", UserSchema);
